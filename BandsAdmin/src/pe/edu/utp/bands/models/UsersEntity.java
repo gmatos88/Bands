a46f0f13547ef3e1ac31ce83d3d7426237b80e55
bannerg <1625238@utp.edu.pe>
2018-02-06 10:51:10 -0500

    UsersEntity v2

--- BandsAdmin/src/pe/edu/utp/bands/models/UsersEntity.java
+++ BandsAdmin/src/pe/edu/utp/bands/models/UsersEntity.java
@@ -12,7 +12,7 @@ public class UsersEntity extends BaseEntity{
     private static String bd    = "bands";
     private static String table = "users";
     private static String DEFAULT_SQL = "SELECT * FROM " + bd + "." + table;
-
+    //    General Method to executeQuery
     private List<User> findByCriteria(String sql) {
         List<User> users;
         if(getConnection() != null) {
@@ -38,6 +38,81 @@ public class UsersEntity extends BaseEntity{
                 return users;
             } catch (SQLException e) {
                 e.printStackTrace();
+            }
+        }
+        return null;
+    }
+    //    General Method to executeUpdate
+    private int updateByCriteria(String sql){
+        if (getConnection() != null){
+            try {
+                return getConnection()
+                        .createStatement()
+                        .executeUpdate(sql);
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return 0;
+    }
+    //    Find All Method
+    public List<User> findAll(){
+        return findByCriteria(DEFAULT_SQL);
+    }
+    //    Find by Id Method
+    public User findById(int id){
+        List<User> users = findByCriteria(
+                DEFAULT_SQL +
+                        "WHERE region_id = " +
+                        String.valueOf(id)
+        );
+        return (users != null ? users.get(0) : null);
+    }
+    //    Find by Name Method
+    public User findByName(String name){
+        List<User> users = findByCriteria(
+                DEFAULT_SQL +
+                        " WHERE region_name = '" + name + "'"
+        );
+        return  (users.isEmpty()) ? null : users.get(0);
+    }
+    //    Find MaxId
+    private int getMaxId(){
+        String sql = "SELECT MAX(id) AS max_id FROM "+table;
+        if (getConnection() != null){
+            try {
+                ResultSet resultSet = getConnection()
+                        .createStatement()
+                        .executeQuery(sql);
+                return resultSet.next() ?
+                        resultSet.getInt(1) : 0;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return 0;
+    }
+    //    Create region
+    public User create(String username, String password, String email, String name, String lastname){
+        if (findByName(username) == null){
+            if (getConnection() != null){
+                String sql = "INSERT INTO "+table+"(id, username, password, email, name, lastname, status, type) " +
+                        "VALUES(" + String.valueOf(getMaxId() + 1) + ", '" +
+                        username + "' , '"+password+"','"+email+"','"+name+"','"+lastname+"', '1', '1'";
+                int results = updateByCriteria(sql);
+                if (results > 0){
+                    User user = new User();
+                    user.setId(getMaxId());
+                    user.setUsername(username);
+                    user.setPassword(password);
+                    user.setEmail(email);
+                    user.setName(name);
+                    user.setLastname(lastname);
+                    user.setStatus(1);
+                    user.setStatus(1);
+                    return user;
+                }
             }
         }
         return null;
