7d504619bcd8ef016c0f88efc67a9b9c1e6bd976
bannerg <1625238@utp.edu.pe>
2018-02-06 10:11:21 -0500

    Base Entity

---
+++ BandsAdmin/src/pe/edu/utp/bands/dto/Users.java
@@ -0,0 +1,62 @@
+package pe.edu.utp.bands.dto;
+
+public class Users {
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+
+    private String name;
+    private String lastname;
+
+    public Users() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+}
